1382b5cb19103b9e35a8dfb2b1e6c0407ad0eb9e
Barrometer <pickup.edward@gmail.com>
2020-05-04 16:52:05 +0100

    Some very basic work + repo work

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
+class NetworkNode{
+  constructor(nodeName,childNodes,parentNodes){   
+    this._nodeName = nodeName //string, should be unique
+    this._childNodes = childNodes //list of all nodes considered children
+    this._parentNodes =  parentNodes //list of all nodes considered parents
+  }
+  addChild(newChildName) {
+    this._childNodes.push(newChildName)
+  }
+  addParent(newParentName){
+    this._parentNodes.push(newParentName)
+  }
+  printChildren(){
+    var x
+    for (i = 0; i < this._childNodes.length(); i ++){
+      x+=this._childNodes[i]+" "
+    }
+    return x
+  }
+  printParents(){
+    var x
+    for (i = 0; i < this._parentNodes.length(); i ++){
+      x+=this._parentNodes[i]+" "
+    }
+    return x
+  }
+}
+nodeA =  new NetworkNode("Node A",[],[])
+nodeB =  new NetworkNode("Node B",[],[])
+nodeA.addChild("Node B")
+nodeB.addParent("Node A")
 console.log("Test")
