843864f72f29e9a4e89b54b2aaa4166ac658d39e
Barrometer <pickup.edward@gmail.com>
2020-05-09 17:08:12 +0100

    Work on rewriting renderer

--- src/render.js
+++ src/render.js
@@ -7,11 +7,53 @@ var network = require("./network");
 var eades = require("./forcedirected");
 var graphgenerator = require("./graphgenerator");
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-var raycaster = new THREE.Raycaster();
-var mouse =  new THREE.Vector2();
 var dat = require("dat.gui");
 
-var gui = new dat.GUI()
+var c1=2,c2=3,c3=3,c4=0.1;
+var numIterations = 30;
+var myGraph = new network.networkGraph();
+var myGenerator = new graphgenerator.dagGenerator(50,1.2);
+myGenerator.randomise();
+var myEades =  new eades.eadesForceSimulator(c1,c2,c3,c4);
+var container;
+var camera,scene,raycaster,renderer,controls;
+var mouse = new THREE.Vector2();
+var params = {
+  c1: 2,
+  c2: 3,
+  c3: 3,
+  c4: 4,
+  iterations: 30
+}
+var myGui = new dat.GUI();
+
+var c1Controller = myGui.add(params,"c1");
+var c2Controller = myGui.add(params,"c2");
+var c3Controller = myGui.add(params,"c3");
+var c4Controller = myGui.add(params,"c4");
+var iterController = myGui.add(params,"iterations");
+
+var nodeArray = [];
+var lineArray = [];
+
+init();
+animate();
+
+function init(){
+  container = document.createElement("div");
+  document.body.appendChild(container);
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight,1,500);
+  scene = new THREE.Scene();
+  raycaster = new THREE.Raycaster();
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  container.appendChild(renderer.domElement);
+  controls = new OrbitControls(camera,renderer.domElement);
+  controls.update();
+  document.addEventListener("mousemove",onMouseMove,false);
+}
+
 function onMouseMove( event ) {
 
 	// calculate mouse position in normalized device coordinates
@@ -21,76 +63,9 @@ function onMouseMove( event ) {
 	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
 }
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
 
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
-var controls = new OrbitControls( camera, renderer.domElement );
-controls.update();
-document.addEventListener('mousemove',onMouseMove,false);
-
-var myGenerator = new graphgenerator.dagGenerator(50,1.2);
-myGenerator.randomise();
-var myGraph =  myGenerator.graph;
-var myEades = new eades.eadesForceSimulator(2,3,3,0.1);
-var i =0; var loops = 200;
-for(i;i<loops;i++){
-  myEades.simulatorStep(myGraph);
-}
-
-myGraph.print();
-var lineMaterial = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-//controls = new THREE.OrbitControls(camera,renderer.domElement)
-//var lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
-var meshMaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-var meshGeometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
-
-var nodeNamesAndCoords = myGraph.getNodeNamesAndCoords();
-var lineNamesAndCoords = myGraph.getLineNamesAndCoords();
-
-function coordsToRenderedNodes(nameAndCoord) {
-  //console.log(nameAndCoord.name);
-  var cube = new THREE.Mesh( meshGeometry, meshMaterial );
-  cube.position.x =  nameAndCoord.coords.x;
-  cube.position.y =  nameAndCoord.coords.y;
-  cube.position.z =  nameAndCoord.coords.z;
-  cube.name = nameAndCoord.name;
-  scene.add(cube);
-}
-function coordsToRenderedArrows(nameAndCoords) {
-  //console.log(nameAndCoords.name);
-  var from = nameAndCoords.coordsFrom;
-  var to = nameAndCoords.coordsTo;
-  //console.log(to.x-from.x)
-  var dir = new THREE.Vector3(to.x - from.x, to.y - from.y, to.z -  from.z);
-  var length = dir.length();
-  dir.normalize();
-  var origin =  new THREE.Vector3(from.x, from.y, from.z);
-  var arrow =  new THREE.ArrowHelper(dir,origin,length,0x0000ff);
-  arrow.name = nameAndCoords.name;
-  scene.add(arrow);
-}
-nodeNamesAndCoords.forEach(coordsToRenderedNodes);
-lineNamesAndCoords.forEach(coordsToRenderedArrows);
-function animate() {
-
-	requestAnimationFrame( animate );
-
-	// required if controls.enableDamping or controls.autoRotate are set to true
-	controls.update();
-
+function animate(){
+  requestAnimationFrame(animate);
+  controls.update();
   raycaster.setFromCamera(mouse,camera);
-  var intersects = raycaster.intersectObjects(scene.children)
-  if(intersects.length > 0){
-    //only care for first
-    console.log("Intersection with object named " + intersects[0].object.name);
-  }
-
-	renderer.render( scene, camera );
-
 }
-animate();
